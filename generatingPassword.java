1b5f0742350c8eecd59c0b3b315f6c39eabe8260
ApexPlayground <115173705+ApexPlayground@users.noreply.github.com>
2023-04-11 22:04:25 +0100

    improved password generator

--- generatingPassword.java
+++ generatingPassword.java
@@ -2,46 +2,51 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class generatingPassword {
+
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
 
-        int choice = 0;
-        do{
+        int reply;
+        do {
+            System.out.println("Enter the length for for password");
+            int length = scan.nextInt();
+            System.out.println(passwordGenerator(length));
+            System.out.println("to generate new password press 1");
+            reply = scan.nextInt();
 
-            // Create a new instance of the Random class
-            Random rand = new Random();
+        } while (reply == 1);
+    }
 
-            // Define strings for each character set: uppercase letters, lowercase letters, special characters, and digits
-            String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            String lower = "abcdefghijklmnopqrstuvwxyz";
-            String special = "!\\\"#$%&'()*+,-./:;<=>?@[\\\\]^_`{|}~";
-            String numbers = "0123456789";
+    public static String passwordGenerator(int length) {
+        String numbers = "1234567890";
+        // Lower case characters
+        String lower = "abcdefghijklmnopqrstuvwzyx";
+        // Uppercase characters5
+        String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        // regex for speacial characters
+        String special = "[!@#$%^&*(),.?\":{}|<>]";
+        // combining all characters in pool variable
+        String pool = lower + upper + special + numbers;
 
-            // Combine the four character sets into a single string
-            String unity = upper + lower + special + numbers;
+        // converting String pool to char values
+        char[] values = pool.toCharArray();
 
-            // Convert the unity string to a character array
-            char[] chars = unity.toCharArray();
-
-            // Shuffle the characters in the array using the Fisher-Yates algorithm
-            for(int i = unity.length() - 1; i > 0;i--){
-                int j = rand.nextInt(i + 1);
-                char temp = chars[i];
-                chars[i] = chars[j];
-                chars[j] = temp;
-            }
-
-            // Generate a random substring of length 10 from the shuffled characters
-            String output = new String(chars).substring(0, 10);
-
-            // Print the final output to the console
-            System.out.println(output);
-
-
-            System.out.println("Do you want to generate new random password \n 1.yes \n 2.no \n choose between 1 or 2 ");
-            choice = scan.nextInt();
+        // looping throught values to randomize it
+        for (int i = values.length - 1; i > 0; i--) {
+            int randSwap = new Random().nextInt(i + 1);
+            char randValue = values[i];
+            values[i] = values[randSwap];
+            values[randSwap] = randValue;
         }
-        while (choice == 1);
+
+        // Creating password of desired length
+        StringBuilder password = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            int index = new Random().nextInt(values.length);
+            password.append(values[index]);
+        }
+
+        return password.toString();
     }
 }
